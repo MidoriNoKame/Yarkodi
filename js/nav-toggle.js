442f483a0b700e3ced7e2a403c8fd4c71358f4e1
MidoriNoKame <83963952+MidoriNoKame@users.noreply.github.com>
2024-08-12 21:14:56 +0300

    yarkodi

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -1,11 +1,25 @@
 $(function() {
 
   /* nav toggle */
+
 $('#nav_toggle').on("click", function(event) {
+    const header = document.querySelector('header');
+    const nav = document.querySelector('.nav');
+
     event.preventDefault();
 
     $(this).toggleClass("active");
     $('#nav').toggleClass("active");
+
+
+    //без jquery, потому что так надо
+    if (nav.classList.contains('active')) {
+        header.style.position = 'fixed';
+        header.classList.add('header-bg-active');
+    } else {
+        header.style.position = 'absolute';
+        header.classList.remove('header-bg-active');
+    }
 });
 
 /* collapse */
