d42a95e2dabda180161255ede802ac1686e90c7a
MidoriNoKame <83963952+MidoriNoKame@users.noreply.github.com>
2025-04-06 19:38:11 +0300

    Yarkodi
    up

--- js/mail-size.js
+++ js/mail-size.js
@@ -1,10 +1,10 @@
 document.querySelectorAll('.clients__block img').forEach(img =>{
     img.onclick = () => {
-        document.querySelector('.pop_up').style.display = 'block';
+        document.querySelector('.pop_up').style.display = 'flex';
         document.querySelector('.pop_up img').src = img.getAttribute('src');
     }
 });
 
-document.querySelector('.pop_up img').onclick = () => {
+document.querySelector('.pop_up').onclick = () => {
     document.querySelector('.pop_up').style.display = 'none';
 }
